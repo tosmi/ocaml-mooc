d70df766fa23115e7166843d81567c0194cd581f
Toni Schmidbauer <toni@stderr.at>
2015-11-21 09:59:04 +0100

    update

--- week1/scratch.ml
+++ week1/scratch.ml
@@ -1,2 +1,8 @@
-1 + 2 * 3 - 4;;
-10 / 3 * 3;;
+let word = "test";;
+let sentence = word ^ "," ^ word in
+    let x = sentence ^ "," ^ sentence in let
+	y = x ^ sentence in y;;
+
+let x = 4+5 in let x =2*x;;
+
+x;;
