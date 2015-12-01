12f7313b31d3c86732fbde72263d1dba21191155
Toni Schmidbauer <toni@stderr.at>
2015-12-01 21:11:52 +0100

    work on ocaml course

--- week4/folding_functions.ml
+++ week4/folding_functions.ml
@@ -12,7 +12,7 @@ The ordering function returns:
     and 0 otherwise.
 
 For the fold_left part, you can use the type 'a option as the
-accumulator: at each iteration of fold_left, if the list if sorted
+accumulator: at each iteration of fold_left, if the list is sorted
 until now, the acccumulator is either Some v, where v is the previous
 element, or None otherwise.
 
@@ -28,8 +28,43 @@ let exists p l =
   List.fold_left (fun a x -> if p x || a then true else false) false l
 ;;
 
+let l1 = [1;2;4;3];;
+let l2 = [1;2;3;4];;
+
+let cmp = fun x y ->
+  if x > y then
+    1
+  else if x < y then
+    -1
+  else
+    0
+
+
 let sorted cmp l =
-  "Replace this stirng with your implementation."
+  let is_sorted = List.fold_left (fun a x ->
+		      match a with
+		      | (Some v) when (cmp x v) <= 0 -> (Some v)
+		      | _ -> None
+		    ) (Some(List.hd l)) l in
+  match is_sorted with
+  | (Some v) -> true;
+  | None     -> false;
+;;
+
+sorted cmp l2;;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 (*
       List.fold_left
